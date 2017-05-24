b1f3747842ff9ee3bc8beed3bc3b8cbfe0087938
KartikSayani9613 <201451030@iiitvadodara.ac.in>
2017-05-24 19:15:36 +0530

    Translating from Xtend to Java

--- FortressTrans/src/FortressToXtend/Sudoko.xtend
+++ FortressTrans/src/FortressToXtend/Sudoko.xtend
@@ -1,5 +1,22 @@
 package FortressToXtend
 
+import java.util.ArrayList
+import java.util.Set
+
 class Sudoko {
+	var private static r = new ArrayList<ArrayList<Set<Integer>>>()
+	def private static Set<Integer> initSet(){
+		var initSet = (1..9).toSet
+		return initSet
+	}
 	
+	def static void main(String[] args) {
+		
+		(0..8).forEach[i|
+			r.add(new ArrayList<Set<Integer>>)
+			(0..8).forEach[j|
+				r.get(i).add(initSet())
+			]
+		]
+	}
 }
--- FortressTrans/xtend-gen/FortressToXtend/Sudoko.java
+++ FortressTrans/xtend-gen/FortressToXtend/Sudoko.java
@@ -1,5 +1,29 @@
 package FortressToXtend;
 
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.function.Consumer;
+import org.eclipse.xtext.xbase.lib.IntegerRange;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+
 @SuppressWarnings("all")
 public class Sudoko {
+  private static ArrayList<ArrayList<Set<Integer>>> r = new ArrayList<ArrayList<Set<Integer>>>();
+  
+  private static Set<Integer> initSet() {
+    Set<Integer> initSet = IterableExtensions.<Integer>toSet(new IntegerRange(1, 9));
+    return initSet;
+  }
+  
+  public static void main(final String[] args) {
+    final Consumer<Integer> _function = (Integer i) -> {
+      ArrayList<Set<Integer>> _arrayList = new ArrayList<Set<Integer>>();
+      Sudoko.r.add(_arrayList);
+      final Consumer<Integer> _function_1 = (Integer j) -> {
+        Sudoko.r.get((i).intValue()).add(Sudoko.initSet());
+      };
+      new IntegerRange(0, 8).forEach(_function_1);
+    };
+    new IntegerRange(0, 8).forEach(_function);
+  }
 }
